9bf382b10db88e55d332e304c6cb98af99ff3e8f
Mrinmay Santra <mrinmay7875@gmail.com>
2021-01-08 13:08:46 +0530

    Added editor and previewer icon and modified the stylings

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -20,33 +20,47 @@ Or _italic_.
 Or... wait for it... **_both!_**
 And feel free to go crazy ~~crossing stuff out~~.
 There's also [links](https://github.com/mrinmay-santra), and lot more interesting things
-
-* And last but not least, let's not forget embedded images:
-![A kitten w/ Text](https://source.unsplash.com/featured/?desk,office)
 `;
 
 function Markdown(props) {
   const [input, setInput] = useState(defaultPlaceholder);
 
   return (
-    <main>
-      <section className="markdown">
-        <textarea
-          className="input"
-          name=""
-          id=""
-          value={input}
-          cols="30"
-          rows="10"
-          onChange={(event) => {
-            setInput(event.target.value);
-          }}
-        ></textarea>
-        <article  className="result">
-          <Reactmarkdown>{input}</Reactmarkdown>
-        </article>
-      </section>
-    </main>
+    <div className="main-container">
+      <div className="markdown-container">
+        {/* <Topbar text="This is editor section" icon={<i class="fas fa-edit fa-lg icons" ></i>} /> */}
+        
+            <div  >
+              <h3 className="topbar-heading" style={{backgroundColor: 'lightblue', padding:"10px",width:"100%"}} >
+                  <i className="fas fa-edit fa-lg icons"></i>This is the editor section</h3>
+            </div>
+            <textarea
+              className="input"
+              name=""
+              id=""
+              value={input}
+              cols="60"
+              rows="25"
+              onChange={(event) => {
+                setInput(event.target.value);
+              }}
+            ></textarea>
+ 
+  </div>
+        <div className="markdown-preview-container">
+
+            <div style={{marginRight:"0px"}}  >
+              <h3 className="topbar-heading" >
+                  <i className="fas fa-tv icons"></i>This is the viewer section</h3>
+            </div>
+
+
+          <div className="result">
+            <Reactmarkdown style={{backgroundColor:"lightblue"}} >{input}</Reactmarkdown>
+          </div>
+        </div>
+     
+    </div>
   );
 }
 
